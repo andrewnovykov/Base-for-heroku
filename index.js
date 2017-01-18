3893e9bab45c83270e2afd0b563f4dba973f3f79
papapin <borabora1212>
2017-01-18 17:11:05 +0200

    add Angular and route files

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
 var express = require('express');
 var app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
+
+
+var users = require('./routes/users');
+
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
+// app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, 'client/dist')));
 
 // views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+// app.set('views', __dirname + '/views');
+// app.set('view engine', 'ejs');
+//
+// app.get('/', function(request, response) {
+//   response.render('client/index');
+// });
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/dist/index.html'));
 });
+
+
+app.use('/api/users', users);
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
---
+++ routes/users.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var router = express.Router();
+var mongojs = require('mongojs');
+// var db = mongojs('mongodb://papapin:123456@ds157247.mlab.com:57247/mytasklist_dron', ['task']);
+
+
+
+module.exports = router;
