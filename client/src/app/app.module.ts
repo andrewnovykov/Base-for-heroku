60974414b2450ccb3afa43819c26d63ccbeea049
papapin <borabora1212>
2017-01-19 10:51:03 +0200

    add client files

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,19 +2,43 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
+import { UserService } from './services/user.service';
+import { TaskComponent } from './task/task.component';
+
+
+const appRoutes = [
+  {
+    path:'',
+    component: LoginComponent
+  },{
+    path:'tasks',
+    component: TaskComponent
+  },{
+    path:'logout',
+    component: LogoutComponent
+  }
+
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    LogoutComponent,
+    TaskComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [ UserService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
